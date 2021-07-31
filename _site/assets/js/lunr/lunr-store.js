be3ee969fd0b298b450cbd4a59e40ea21cc5478c
Itsbeenalongday <dbtjdals1771@ajou.ac.kr>
2021-08-01 02:51:16 +0900

    modify setting

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "setting",
-        "excerpt":"참고한 블로그 및 코드 github setting blog github code How to Run? $ jekyll serve --livereload # hot-reload option step by step 최상위는 index.html _data 디렉토리의 navigation.yml에서 네비게이션 바를 설정할 수 있다. # main links main: - title: \"연도별 포스트\" url: /posts/ - title: \"카테고리\" url: /categories/ - title: \"태그\"...","categories": ["Blog"],
+        "excerpt":"How to Run? 1 $ jekyll serve --livereload # hot-reload option step by step 최상위는 index.html _data 디렉토리의 navigation.yml에서 네비게이션 바를 설정할 수 있다. # main links main: - title: \"연도별 포스트\" url: /posts/ - title: \"카테고리\" url: /categories/ - title: \"태그\" url: /tags/ _pages 디렉토리에서 위에서 설정한 navigation url...","categories": ["Blog"],
         "tags": ["blog","jekyll"],
         "url": "/blog/setting/",
         "teaser": null

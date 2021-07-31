8df0f5fa1e11a8df1c163a95bbd2c3c9f83b5133
Itsbeenalongday <dbtjdals1771@ajou.ac.kr>
2021-08-01 01:29:58 +0900

    test post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "test",
+        "excerpt":"유의사항   const a = 1;  ","categories": ["Blog"],
+        "tags": [],
+        "url": "/blog/test/",
+        "teaser": null
+      }]

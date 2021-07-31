ad9c71625c6f9b0a2b0be0bf97568c7b0a74bbb4
Itsbeenalongday <dbtjdals1771@ajou.ac.kr>
2021-08-01 02:29:54 +0900

    to write about the setting [2021-08-01]

--- _pages/year-archive.md
+++ _pages/year-archive.md
@@ -1,5 +1,5 @@
 ---
-title: "Posts by Year"
+title: "년도 별"
 permalink: /posts/
 layout: posts
 author_profile: true
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,7 @@
 var store = [{
-        "title": "test",
-        "excerpt":"유의사항   const a = 1;  ","categories": ["Blog"],
-        "tags": [],
-        "url": "/blog/test/",
+        "title": "setting",
+        "excerpt":"참고한 블로그 github setting step by step 최상위 index.html _data 디렉토리의 navigation.yml에서 네비게이션 바를 설정할 수 있다. # main links main: - title: \"연도별 포스트\" url: /posts/ - title: \"카테고리\" url: /categories/ - title: \"태그\" url: /tags/ _pages 디렉토리에서 위에서 설정한 navigation url 매핑이 된다. # 카테고리 예시 --- title:...","categories": ["Blog"],
+        "tags": ["blog","jekyll"],
+        "url": "/blog/setting/",
         "teaser": null
       }]

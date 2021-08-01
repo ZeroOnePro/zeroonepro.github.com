e065c8439349403e40474b9349faeabce55ae8f9
Itsbeenalongday <dbtjdals1771@ajou.ac.kr>
2021-08-01 13:49:20 +0900

    some change

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,7 +2,7 @@ var store = [{
         "title": "setting",
         "excerpt":"How to Run? 1 $ jekyll serve --livereload # hot-reload option step by step 최상위는 index.html _data 디렉토리의 navigation.yml에서 네비게이션 바를 설정할 수 있다. # main links main: - title: \"연도별 포스트\" url: /posts/ - title: \"카테고리\" url: /categories/ - title: \"태그\" url: /tags/ _pages 디렉토리에서 위에서 설정한 navigation url...","categories": ["Blog"],
         "tags": ["blog","jekyll"],
-        "url": "/blog/setting/",
+        "url": "/blog/Jekyll-%EB%B8%94%EB%A1%9C%EA%B7%B8-%EC%84%B8%ED%8C%85%ED%95%98%EA%B8%B0/",
         "teaser": null
       },{
         "title": "개발 환경 설정",

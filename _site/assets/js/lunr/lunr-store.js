14022010bb0defaadf5e0d18e97b02346e0ef400
Itsbeenalongday <dbtjdals1771@ajou.ac.kr>
2021-08-22 17:08:31 +0900

    add chapter 3 and fix images

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -11,51 +11,69 @@ var store = [{
         "url": "/blog/Jekyll-%EB%B8%94%EB%A1%9C%EA%B7%B8-%EC%84%B8%ED%8C%85%ED%95%98%EA%B8%B0/",
         "teaser": null
       },{
-        "title": "개발 환경 설정",
-        "excerpt":"1.4 개발 환경 설정하기 Node 설치 # mac $ brew install node # linux $ sudo apt-get update $ sudo apt-get install -y build-essential $ sudo apt-get intall curl $ curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -- $ sudo apt-get install -y nodejs 설치 확인 $ node -v v16.4.1...","categories": ["Node.js"],
-        "tags": ["Node.js","theory"],
-        "url": "/node.js/%EA%B0%9C%EB%B0%9C-%ED%99%98%EA%B2%BD-%EC%84%A4%EC%A0%95/",
-        "teaser": null
-      },{
-        "title": "서버 외의 노드",
-        "excerpt":"1.3 서버 외의 노드 처음에는 노드를 대부분 서버로 사용했지만, 노드는 자바스크립트 런타임이므로 용도가 서버에만 한정 되지 않는다. 사용범위가 점점 늘어나서 노드는 웹, 모바일, 데스크톱 애플리케이션 개발에도 사용되기 시작하였다. 노드 기반으로 돌아가는 대표적인 웹 프레임워크로는 앵귤러, 리액트, 리액트 네이티브, 뷰 등이 있다. 데스크톱 개발 도구로는 일렉트론이 대표적인데, 이걸로 만들어진 프로그램으로는 vscode,...","categories": ["Node.js"],
-        "tags": ["Node.js","theory"],
-        "url": "/node.js/%EC%84%9C%EB%B2%84-%EC%99%B8%EC%9D%98-%EB%85%B8%EB%93%9C/",
-        "teaser": null
-      },{
-        "title": "서버로서의 노드",
-        "excerpt":"1.2 서버로서의 노드 노드는 기본적으로 싱글 스레드, 논 블로킹 모델을 사용한다.(js의 특성이기도 하다) 노드 서버 또한 동일한 모델일 수 밖에 없다. 따라서 노드 서버의 장단점은 싱글 스레드, 논 블로킹 모델의 장단점과 크게 다르지 않다. 서버에는 기본적으로 I / O 요청이 많이 발생하므로, I / O 처리를 잘하는 노드를 서버로 사용하면...","categories": ["Node.js"],
-        "tags": ["Node.js","theory"],
-        "url": "/node.js/%EC%84%9C%EB%B2%84%EB%A1%9C%EC%84%9C%EC%9D%98-%EB%85%B8%EB%93%9C/",
-        "teaser": null
-      },{
-        "title": "참고할 만한 사이트 모음",
-        "excerpt":"1.5 참고할 만한 사이트 모음      노드 공식 사이트: https://nodejs.org/ko   노드 공식 사이트의 가이드: https://nodejs.org/ko/docs/guides/   노드에 대한 전반적인 설명: https://nodejs.dev/   이벤트 루프 설명: https://nodejs.org/ko/docs/guides/event-loop-timers-and-nexttick/   이벤트 루프에 대한 시각적 설명: http://latentflip.com/loupe   (Node.js 교과서 개정 2판 - 길벗, 2021)을 학습하고 개인 학습용으로 정리한 내용입니다.  ","categories": ["Node.js"],
-        "tags": ["Node.js","theory"],
-        "url": "/node.js/%EC%B0%B8%EA%B3%A0%ED%95%A0-%EB%A7%8C%ED%95%9C-%EC%82%AC%EC%9D%B4%ED%8A%B8-%EB%AA%A8%EC%9D%8C/",
-        "teaser": null
-      },{
-        "title": "핵심 개념 이해하기",
+        "title": "1.1 핵심 개념 이해하기",
         "excerpt":"1.1 핵심 개념 이해하기 1.1.1 정의 Node.js는 chrome V8 자바스크립트 엔진으로 빌드된 자바스크립트 런타임이다. Node.js는 이벤트 기반, 논 블로킹 I / O 모델을 사용해 가볍고 효율적이다. 자바스크립트 엔진 자바스크립트 엔진(JavaScript engine)은 자바스크립트 코드를 실행하는 프로그램 또는 인터프리터이다. 자바스크립트 엔진은 전통적인 인터프리터일 수도 있고, 특정한 방식으로 바이트코드로 JIT 컴파일을 할 수...","categories": ["Node.js"],
         "tags": ["Node.js","theory"],
-        "url": "/node.js/%ED%95%B5%EC%8B%AC-%EA%B0%9C%EB%85%90-%EC%9D%B4%ED%95%B4%ED%95%98%EA%B8%B0/",
+        "url": "/node.js/1.1-%ED%95%B5%EC%8B%AC-%EA%B0%9C%EB%85%90-%EC%9D%B4%ED%95%B4%ED%95%98%EA%B8%B0/",
         "teaser": null
       },{
-        "title": "async await",
-        "excerpt":"2.1.8 async / await promise가 콜백 지옥을 해결해줬지만 여전히 코드는 장황하다. then과 catch가 계속 반복되기 때문이다. async / await 문법은 프로미스를 사용한 코드를 한 번 더 깔끔하게 줄여줄 수 있다. const findAndSaveUser = (Users) =&gt; { Users.findOne({}) .then((user) =&gt; { user.name = 'you'; return user.save(); // 이는 프로미스 객체를 리턴해야...","categories": ["Node.js"],
+        "title": "1.2 서버로서의 노드",
+        "excerpt":"노드는 기본적으로 싱글 스레드, 논 블로킹 모델을 사용한다.(js의 특성이기도 하다) 노드 서버 또한 동일한 모델일 수 밖에 없다. 따라서 노드 서버의 장단점은 싱글 스레드, 논 블로킹 모델의 장단점과 크게 다르지 않다. 서버에는 기본적으로 I / O 요청이 많이 발생하므로, I / O 처리를 잘하는 노드를 서버로 사용하면 좋다. 노드는 논...","categories": ["Node.js"],
         "tags": ["Node.js","theory"],
-        "url": "/node.js/aysnc-await/",
+        "url": "/node.js/1.2-%EC%84%9C%EB%B2%84%EB%A1%9C%EC%84%9C%EC%9D%98-%EB%85%B8%EB%93%9C/",
         "teaser": null
       },{
-        "title": "프로미스",
+        "title": "1.3 서버 외의 노드",
+        "excerpt":"1.3 서버 외의 노드 처음에는 노드를 대부분 서버로 사용했지만, 노드는 자바스크립트 런타임이므로 용도가 서버에만 한정 되지 않는다. 사용범위가 점점 늘어나서 노드는 웹, 모바일, 데스크톱 애플리케이션 개발에도 사용되기 시작하였다. 노드 기반으로 돌아가는 대표적인 웹 프레임워크로는 앵귤러, 리액트, 리액트 네이티브, 뷰 등이 있다. 데스크톱 개발 도구로는 일렉트론이 대표적인데, 이걸로 만들어진 프로그램으로는 vscode,...","categories": ["Node.js"],
+        "tags": ["Node.js","theory"],
+        "url": "/node.js/1.3-%EC%84%9C%EB%B2%84-%EC%99%B8%EC%9D%98-%EB%85%B8%EB%93%9C/",
+        "teaser": null
+      },{
+        "title": "1.4 개발 환경 설정",
+        "excerpt":"1.4 개발 환경 설정하기 Node 설치 # mac $ brew install node # linux $ sudo apt-get update $ sudo apt-get install -y build-essential $ sudo apt-get intall curl $ curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -- $ sudo apt-get install -y nodejs 설치 확인 $ node -v v16.4.1...","categories": ["Node.js"],
+        "tags": ["Node.js","theory"],
+        "url": "/node.js/1.4-%EA%B0%9C%EB%B0%9C-%ED%99%98%EA%B2%BD-%EC%84%A4%EC%A0%95/",
+        "teaser": null
+      },{
+        "title": "1.5 참고할 만한 사이트 모음",
+        "excerpt":"1.5 참고할 만한 사이트 모음      노드 공식 사이트: https://nodejs.org/ko   노드 공식 사이트의 가이드: https://nodejs.org/ko/docs/guides/   노드에 대한 전반적인 설명: https://nodejs.dev/   이벤트 루프 설명: https://nodejs.org/ko/docs/guides/event-loop-timers-and-nexttick/   이벤트 루프에 대한 시각적 설명: http://latentflip.com/loupe   (Node.js 교과서 개정 2판 - 길벗, 2021)을 학습하고 개인 학습용으로 정리한 내용입니다.  ","categories": ["Node.js"],
+        "tags": ["Node.js","theory"],
+        "url": "/node.js/1.5-%EC%B0%B8%EA%B3%A0%ED%95%A0-%EB%A7%8C%ED%95%9C-%EC%82%AC%EC%9D%B4%ED%8A%B8-%EB%AA%A8%EC%9D%8C/",
+        "teaser": null
+      },{
+        "title": "2.1 프로미스",
         "excerpt":"2.1.7 프로미스 자바스크립트와 노드에서는 주로 비동기를 접한다. 특히 이벤트 리스너를 사용할 때 콜백 함수를 자주 사용한다. ES6 부터는 자바스크립트와 노드의 API들이 콜백 대신 프로미스기반으로 재구성되며, 악명 높은 콜백지옥현상을 극복했다는 평가를 받고 있다. 프로미스는 반드시 알아두자. 프로미스는 다음과 같은 규칙이 있다. 먼저 프로미스 객체를 생성해야 한다. const condition = true; const...","categories": ["Node.js"],
         "tags": ["Node.js","theory"],
-        "url": "/node.js/%ED%94%84%EB%A1%9C%EB%AF%B8%EC%8A%A4/",
+        "url": "/node.js/2.1-%ED%94%84%EB%A1%9C%EB%AF%B8%EC%8A%A4/",
         "teaser": null
       },{
-        "title": "프론트엔드 자바스크립트",
-        "excerpt":"2.2 프론트엔드 자바스크립트 2.2.1 AJAX Asynchronous Javascript And Xml는 비동기적 웹 서비스를 개발할 때 사용하는 기법이다. 이름에 XML이 들어 있지만 꼭 이것을 사용해야 하는 것은 아니며, 요즘에는 JSON을 많이 사용한다. 쉽게 말해 페이지 이동 없이 서버에 요청을 보내고 응답을 받는 기술이다. 웹 사이트 중 페이지 전환 없이 새로운 데이터를 불러오는...","categories": ["Node.js"],
+        "title": "2.2 async await",
+        "excerpt":"2.1.8 async / await promise가 콜백 지옥을 해결해줬지만 여전히 코드는 장황하다. then과 catch가 계속 반복되기 때문이다. async / await 문법은 프로미스를 사용한 코드를 한 번 더 깔끔하게 줄여줄 수 있다. const findAndSaveUser = (Users) =&gt; { Users.findOne({}) .then((user) =&gt; { user.name = 'you'; return user.save(); // 이는 프로미스 객체를 리턴해야...","categories": ["Node.js"],
         "tags": ["Node.js","theory"],
-        "url": "/node.js/%ED%94%84%EB%A1%A0%ED%8A%B8%EC%97%94%EB%93%9C-%EC%9E%90%EB%B0%94%EC%8A%A4%ED%81%AC%EB%A6%BD%ED%8A%B8/",
+        "url": "/node.js/2.2-aysnc-await/",
+        "teaser": null
+      },{
+        "title": "2.3 프론트엔드 자바스크립트",
+        "excerpt":"2.3 프론트엔드 자바스크립트 2.2.1 AJAX Asynchronous Javascript And Xml는 비동기적 웹 서비스를 개발할 때 사용하는 기법이다. 이름에 XML이 들어 있지만 꼭 이것을 사용해야 하는 것은 아니며, 요즘에는 JSON을 많이 사용한다. 쉽게 말해 페이지 이동 없이 서버에 요청을 보내고 응답을 받는 기술이다. 웹 사이트 중 페이지 전환 없이 새로운 데이터를 불러오는...","categories": ["Node.js"],
+        "tags": ["Node.js","theory"],
+        "url": "/node.js/2.3-%ED%94%84%EB%A1%A0%ED%8A%B8%EC%97%94%EB%93%9C-%EC%9E%90%EB%B0%94%EC%8A%A4%ED%81%AC%EB%A6%BD%ED%8A%B8/",
+        "teaser": null
+      },{
+        "title": "3.1 REPL 사용하기",
+        "excerpt":"자바스크립트는 스크립트 언어이므로 미리 컴파일을 하지 않아도 즉석에서 코드를 실행할 수 있다. 노드도 콘솔을 제공하는데, 입력한 코드를 읽고(Read), 해석하고(Eval), 결과물을 출력하고(Print), 종료 시 까지 반복한다.(Loop)라고 해서 REPL이라고 부른다. 노드의 REPL을 사용해보자 터미널에 node라고 치면 실행가능하다. 또한 node file.js 이런 식으로 파일 실행도 가능하다. (Node.js 교과서 개정 2판 - 길벗, 2021)을...","categories": ["Node.js"],
+        "tags": ["Node.js","theory"],
+        "url": "/node.js/3.1-REPL-%EC%82%AC%EC%9A%A9%ED%95%98%EA%B8%B0/",
+        "teaser": null
+      },{
+        "title": "3.2 모듈로 만들기",
+        "excerpt":"노드는 코드를 모듈로 만들 수 있다는 점에서 브라우저의 자바스크립트와 다르다. 모듈이란 특정한 기능을 하는 함수나 변수들의 집합니다. 예를 들면 수학에 관련된 코드들만 모아서 모듈을 하나 만들 수 있다. 모듈은 자체로도 하나의 프로그램이면서 다른 프로그램의 부품으로도 사용할 수 있다. 모듈로 만들어두면 여러 프로그램에 해당 모듈을 재사용할 수 있다. 자바스크립트에서 코드를 재사용하기...","categories": ["Node.js"],
+        "tags": ["Node.js","theory"],
+        "url": "/node.js/3.2-%EB%AA%A8%EB%93%88%EB%A1%9C-%EB%A7%8C%EB%93%A4%EA%B8%B0/",
+        "teaser": null
+      },{
+        "title": "3.3 노드 내장 객체 알아보기",
+        "excerpt":"3.3 노드 내장 객체 알아보기 노드에서는 기본적인 내장 객체와 내장 모듈을 제공한다. 따로 설치하지 않아도 가능하고, 브라우저의 window 객체와 유사하다. 3.3.1) global global 객체는 전역 객체로써 브라우저의 window와 비슷하다. 전역 객체이므로 모든 파일에서 접근할 수 있다. 또한, window.open 메서드를 window를 생략하고 open으로 호출 할 수 있는 것 처럼 global도 생략...","categories": ["Node.js"],
+        "tags": ["Node.js","theory"],
+        "url": "/node.js/3.3-%EB%85%B8%EB%93%9C-%EB%82%B4%EC%9E%A5-%EA%B0%9D%EC%B2%B4-%EC%95%8C%EC%95%84%EB%B3%B4%EA%B8%B0/",
         "teaser": null
       }]
